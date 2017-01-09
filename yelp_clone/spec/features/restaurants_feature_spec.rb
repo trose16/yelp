2c348ba897a0f46fb1068b57d0d6b232f1deb717
sara <sara@huhbub.com>
2017-01-09 18:11:58 +0000

    step 10: adds description

--- yelp_clone/spec/features/restaurants_feature_spec.rb
+++ yelp_clone/spec/features/restaurants_feature_spec.rb
@@ -19,7 +19,7 @@ feature 'restaurants' do
 
    scenario 'display restuarants' do
      visit '/restaurants'
-     expect(page).to have_content('KFC')
+     expect(page).to have_content("KFC")
      expect(page).not_to have_content ('No restaurants yet')
     end
   end
@@ -37,13 +37,31 @@ feature 'restaurants' do
 
   context 'viewing restaurants' do
 
-    let!(:kfc){ Restaurant.create(name: 'KFC') }
+    let!(:kfc){ Restaurant.create(name: 'KFC', description: 'Deep fried goodness') }
 
     scenario 'lets a user view a restaurant' do
       visit '/restaurants'
       click_link 'KFC'
       expect(page).to have_content 'KFC'
+      expect(page).to have_content 'Deep fried goodness'
       expect(current_path).to eq "/restaurants/#{kfc.id}"
     end
   end
+
+  context 'editing restaurants' do
+
+    before { Restaurant.create name: 'KFC', description: 'Deep fried goodness', id: 1 }
+    scenario 'let a user edit a restaurant' do
+      visit '/restaurants'
+      click_link 'Edit KFC'
+      fill_in 'Name', with: 'Kentucky Fried Chicken'
+      fill_in 'Description', with: 'Deep fried goodness'
+      click_button 'Update Restaurant'
+      click_link 'Kentucky Fried Chicken'
+      expect(page).to have_content 'Kentucky Fried Chicken'
+      expect(page).to have_content 'Deep fried goodness'
+      expect(current_path).to eq '/restaurants/1'
+    end
+  end
+
 end
