90197557de36eeaa764a281efb22337100c8cac4
sara <sara@huhbub.com>
2017-01-10 20:33:39 +0000

    step 14: adds validation for restaurant uniqueness

--- yelp_clone/spec/models/restaurant_spec.rb
+++ yelp_clone/spec/models/restaurant_spec.rb
@@ -7,4 +7,10 @@ it 'is not valid with a name of less than three characters' do
   expect(restaurant).to have(1).error_on(:name)
   expect(restaurant).not_to be_valid
   end
+
+  it "is not valid unless it has a unique name" do
+    Restaurant.create(name: "Moe's Tavern")
+    restaurant = Restaurant.new(name: "Moe's Tavern")
+    expect(restaurant).to have(1).error_on(:name)
+  end
 end
