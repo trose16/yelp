2bd77d98acabd9cd09661bb4a45508c0cc0018e2
sara <sara@huhbub.com>
2017-01-10 20:25:55 +0000

    step 13: tests validations

--- yelp_clone/spec/features/restaurants_feature_spec.rb
+++ yelp_clone/spec/features/restaurants_feature_spec.rb
@@ -75,6 +75,17 @@ feature 'restaurants' do
     expect(page).to have_content 'Restaurant deleted successfully'
   end
 
+  context 'an invalid restaurant' do
+    scenario 'does not let you submit a name that is too short' do
+      visit '/restaurants'
+      click_link 'Add a restaurant'
+      fill_in 'Name', with: 'kf'
+      click_button 'Create Restaurant'
+      expect(page).not_to have_css 'h2', text: 'kf'
+      expect(page).to have_content 'error'
+    end
+  end
+
 end
 
 end
