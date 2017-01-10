ee27dd81126a085c3de76d2f191d5a5c99a79b46
Teako Warfield-Graham <teakowg@gmail.com>
2017-01-10 14:54:50 +0000

    Validations for restaurant name

--- yelp_clone/spec/features/restaurants_feature_spec.rb
+++ yelp_clone/spec/features/restaurants_feature_spec.rb
@@ -73,5 +73,19 @@ feature 'restaurants' do
     end
   end
 
+  context 'creating restaurants' do
+
+    context 'an invalid restaurant' do
+      scenario 'does not let you submt a name that is too short' do
+        visit '/restaurants'
+        click_link 'Add a restaurant'
+        fill_in 'Name', with: 'kf'
+        click_button 'Create Restaurant'
+        expect(page).not_to have_css 'h2', text: 'kf'
+        expect(page).to have_content 'error'
+      end
+    end
+  end
+
 
 end
