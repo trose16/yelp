e903b7a9ee48a83e9c0e51597b1a050be9920108
Oscar Barlow <oscar.barlow@gmail.com>
2017-01-13 14:53:03 +0000

    TDD creating review with user, using module

--- yelp_clone/app/models/restaurant.rb
+++ yelp_clone/app/models/restaurant.rb
@@ -1,6 +1,6 @@
 class Restaurant < ApplicationRecord
   validates :name, length: { minimum: 3 }, uniqueness: true
-  has_many :reviews, dependent: :destroy
+  has_many :reviews, -> { extending WithUserAssociationExtension }, dependent: :destroy
   belongs_to :user
 
 end
--- yelp_clone/spec/models/review_spec.rb
+++ yelp_clone/spec/models/review_spec.rb
@@ -1,13 +1,30 @@
 require 'rails_helper'
 
 describe Review, type: :model do
+
+  it { is_expected.to belong_to :user }
+  it { is_expected.to belong_to :restaurant }
+
   it "is invalid if the rating is more than 5" do
     review = Review.new(rating: 10)
     expect(review).to have(1).error_on(:rating)
   end
 
-  it { is_expected.to belong_to :user }
+  describe 'build_with_user' do
 
-  it { is_expected.to belong_to :restaurant }
+      let(:user) { User.create email: 'test@test.com', password: "test123" }
+      let(:restaurant) { Restaurant.create name: 'Test' }
+      let(:review_params) { {rating: 5, thoughts: 'yum'} }
+
+      subject(:review) { restaurant.reviews.build_with_user(review_params, user) }
+
+      it 'builds a review' do
+        expect(review).to be_a Review
+      end
+
+      it 'builds a review associated with the specified user' do
+        expect(review.user).to eq user
+      end
+    end
 
 end
